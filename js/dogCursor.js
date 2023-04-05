7ea92119308cd85f0faa3b9cbc53be3a143a87ac
Haruka Yagi <1127.yagiha@ezweb.ne.jp>
2023-04-05 21:58:53 +0900

    cursor add

---
+++ js/dogCursor.js
@@ -0,0 +1,36 @@
+//マウスストーカー用のdivを取得
+const stalker = document.getElementById('stalker');
+
+const stalkerJust = document.getElementById('stalker_just_wrapp');
+
+const stopBtn = document.getElementById('stop_btn');
+
+
+stopBtn.addEventListener('click', function () {
+
+    stalker.classList.toggle("cursor_move");
+
+
+    //マウスに追従させる処理
+    document.addEventListener('mousemove', function (e) {
+        if (stalker.classList.contains("cursor_move")) {
+            stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+        } else {
+            stalker.style.transform = 'translate(0)';
+        }
+    });
+
+    document.addEventListener('mousemove', function (f) {
+        if (stalker.classList.contains("cursor_move")) {
+            stalkerJust.style.transform = 'translate(' + f.clientX + 'px, ' + f.clientY + 'px)';
+        } else {
+            stalkerJust.style.transform = 'translate(0)';
+        }
+
+    });
+
+
+
+
+});
+
